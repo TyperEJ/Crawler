47019f64cc763817e4680bf44f5abf9133f15a2e
EJLin <z8892463@gmail.com>
2019-09-13 01:29:53 +0800

    fixed board

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Enums\BoardListEnum;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,14 +25,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('crawler:start', ['Lifeismoney'])
-            ->everyMinute();
-        $schedule->command('crawler:start', ['MacShop'])
-            ->everyMinute();
-        $schedule->command('crawler:start', ['MobilePay'])
-            ->everyMinute();
-        $schedule->command('crawler:start', ['soho'])
-            ->everyMinute();
+        foreach (BoardListEnum::getList() as $board){
+            $schedule->command('crawler:start', [$board])
+                ->withoutOverlapping()
+                ->everyMinute()
+                ->runInBackground();
+        }
     }
 
     /**
--- app/Enums/BoardListEnum.php
+++ app/Enums/BoardListEnum.php
@@ -8,6 +8,34 @@ class BoardListEnum
     const LIFE_IS_MONEY = 'Lifeismoney';
     const MOBILE_PAY = 'MobilePay';
     const SOHO = 'soho';
+    const IOS = 'iOS';
+    const STEAM = 'Steam';
+    const ACTUARY = 'Actuary';
+    const BUY_TOGETHER = 'BuyTogether';
+    const DRAMA_TICKET = 'Drama-Ticket';
+    const CREDITCARD = 'CREDITCARD';
+    const HARDWARE_SALE = 'HardwareSale';
+    const MOBILESALES = 'mobilesales';
+    const GAMESALE = 'Gamesale';
+    const PC_SHOPPING = 'PC_Shopping';
+    const BABY_MOTHER = 'BabyMother';
+    const MOBILE_COMM = 'MobileComm';
+    const FORSALE = 'forsale';
+    const INSTANT_MESS = 'Instant_Mess';
+    const MOVIE = 'movie';
+    const DC_SALE = 'DC_SALE';
+    const E_COUPON = 'e-coupon';
+    const TAICHUNG_BUN = 'TaichungBun';
+    const CVS = 'CVS';
+    const HSINCHU = 'Hsinchu';
+    const JOB = 'job';
+    const TAINAN = 'Tainan';
+    const BABY_PRODUCTS = 'BabyProducts';
+    const E_SHOPPING = 'e-shopping';
+    const NB_SHOPPING = 'nb-shopping';
+    const PLAY_STATION = 'PlayStation';
+    const HOME_SALE = 'home-sale';
+    const CODE_JOB = 'CodeJob';
 
     public static function getList()
     {
@@ -16,6 +44,34 @@ class BoardListEnum
             self::LIFE_IS_MONEY,
             self::MOBILE_PAY,
             self::SOHO,
+            self::IOS,
+            self::STEAM,
+            self::ACTUARY,
+            self::BUY_TOGETHER,
+            self::DRAMA_TICKET,
+            self::CREDITCARD,
+            self::HARDWARE_SALE,
+            self::MOBILESALES,
+            self::GAMESALE,
+            self::PC_SHOPPING,
+            self::BABY_MOTHER,
+            self::MOBILE_COMM,
+            self::FORSALE,
+            self::INSTANT_MESS,
+            self::MOVIE,
+            self::DC_SALE,
+            self::E_COUPON,
+            self::TAICHUNG_BUN,
+            self::CVS,
+            self::HSINCHU,
+            self::JOB,
+            self::TAINAN,
+            self::BABY_PRODUCTS,
+            self::E_SHOPPING,
+            self::NB_SHOPPING,
+            self::PLAY_STATION,
+            self::HOME_SALE,
+            self::CODE_JOB,
         ];
     }
 }
