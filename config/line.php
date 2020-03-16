f6d437d9f4d7ff7a8b8872db381c6fec7a245313
EJLin <z8892463@gmail.com>
2020-03-16 11:40:46 +0800

    fixed config

--- config/line.php
+++ config/line.php
@@ -2,16 +2,16 @@
 
 return [
     'line_bot' => [
-        'channel_id' => '1613982834',
-        'channel_secret' => '050d865c11d693b92cfbf0fa4423cc9f',
-        'channel_token' => '834yhTW/WKSjSO8ajoM4rE0Zo2yTf03GUsrH5JzKHXx4Td66Ojxp2LI6ajRKPcnVwI7++xBNswXAGr2tucLjq2zhT6VONkdTxiegwx9dWvvA7vzZZi41qnJMhHBpZ0lGMxM+khT6m7s0h6JS1/QR8wdB04t89/1O/w1cDnyilFU=',
+        'channel_id' => '',
+        'channel_secret' => '',
+        'channel_token' => '',
     ],
     'line_login' => [
-        'channel_id' => '1619553356',
-        'channel_secret' => 'c93613d4c06ad8ddec341e79c332a36e'
+        'channel_id' => '',
+        'channel_secret' => ''
     ],
     'line_notify' => [
-        'channel_id' => 'akNuG9fEsdebVAQpGShJ8a',
-        'channel_secret' => 'aQAPNfp0lCTIHSoqQ9gjVETelxlyIJIitgk5V27zOtE'
+        'channel_id' => '',
+        'channel_secret' => ''
     ]
 ];
