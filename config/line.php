6a1aef9cc649400b8b52f60739ca022a77a66f8f
EJLin <z8892463@gmail.com>
2020-06-14 20:01:04 +0800

    fixed config

--- config/line.php
+++ config/line.php
@@ -2,16 +2,16 @@
 
 return [
     'line_bot' => [
-        'channel_id' => '',
-        'channel_secret' => '',
-        'channel_token' => '',
+        'channel_id' => env('LINE_BOT_CHANNEL_ID'),
+        'channel_secret' => env('LINE_BOT_CHANNEL_SECRET'),
+        'channel_token' => env('LINE_BOT_CHANNEL_TOKEN'),
     ],
     'line_login' => [
-        'channel_id' => '',
-        'channel_secret' => ''
+        'channel_id' => env('LINE_LOGIN_CHANNEL_ID'),
+        'channel_secret' => env('LINE_LOGIN_CHANNEL_SECRET'),
     ],
     'line_notify' => [
-        'channel_id' => '',
-        'channel_secret' => ''
+        'channel_id' => env('LINE_NOTIFY_CHANNEL_ID'),
+        'channel_secret' => env('LINE_NOTIFY_CHANNEL_SECRET'),
     ]
 ];
