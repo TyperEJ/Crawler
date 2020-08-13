c1294964b1ab45843fdc67fdd21c83fc32487282
EJLin <z8892463@gmail.com>
2020-08-13 16:34:15 +0800

    fixed welcome page main text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    This is EJLin啦。
                 </div>
 
                 <div class="links">
